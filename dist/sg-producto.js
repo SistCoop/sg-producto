717279008e0445d62469dcc42ba97ecb46eb360a
Carlos <carlosthe19916@gmail.com>
2015-04-28 11:18:10 -0500

    CRED-21 add SGProductoTasa

--- dist/sg-producto.js
+++ dist/sg-producto.js
@@ -186,6 +186,52 @@
 
     }]);
 
+    module.factory('SGProductoTasa', ['ProductoRestangular',  function(ProductoRestangular) {
+
+        var url = 'productoTasas';
+        var urlCount = url + '/count';
+
+        var modelMethos = {
+            $new: function(id){
+                return angular.extend({id: id}, modelMethos);
+            },
+            $build: function(){
+                return angular.extend({id: undefined}, modelMethos, {$save: function(){
+                    return ProductoRestangular.all(url).post(this);
+                }});
+            },
+            $save: function() {
+                return ProductoRestangular.one(url, this.id).customPUT(ProductoRestangular.copy(this),'',{},{});
+            },
+
+            $find: function(id){
+                return ProductoRestangular.one(url, id).get();
+            },
+            $search: function(queryParams){
+                return ProductoRestangular.all(url).getList(queryParams);
+            },
+
+            $count: function(){
+                return ProductoRestangular.one(urlCount).get();
+            },
+
+            $remove: function(id){
+                return ProductoRestangular.one(url, id).remove();
+            }
+        };
+
+        ProductoRestangular.extendModel(url, function(obj) {
+            if(angular.isObject(obj)) {
+                return angular.extend(obj, modelMethos);
+            } else {
+                return angular.extend({id: obj}, modelMethos)
+            }
+        });
+
+        return modelMethos;
+
+    }]);
+
     module.factory('SGProductoCaracteristica', ['ProductoRestangular',  function(ProductoRestangular) {
 
         var url = 'productoCaracteristicas';
